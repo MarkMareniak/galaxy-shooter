25d52af3dafb4f4b34cd227101c15ad64b77fce7
DESKTOP-TJTEG8U\Happy <mark.silva@happygrupo.com.br>
2023-02-08 14:53:00 -0300

    Movimentos do personagem

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveX = Input.GetAxis("Horizontal");
+        var moveY = Input.GetAxis("Vertical");
+        transform.Translate(moveX * speed * Vector3.right * Time.deltaTime);
+        transform.Translate(moveY * speed * Vector3.up * Time.deltaTime);
+    }
+}
