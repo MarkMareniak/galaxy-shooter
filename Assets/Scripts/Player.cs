05fd2e9d5cb8c86a1515ec60a435cf10b0b8f795
DESKTOP-TJTEG8U\Happy <mark.silva@happygrupo.com.br>
2023-02-09 14:26:27 -0300

    Implementado limite de tela para o personagem

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,5 +18,33 @@ public class Player : MonoBehaviour
         var moveY = Input.GetAxis("Vertical");
         transform.Translate(moveX * speed * Vector3.right * Time.deltaTime);
         transform.Translate(moveY * speed * Vector3.up * Time.deltaTime);
+
+        if (transform.position.x > 7.013509f)
+        {
+            var newPos = transform.position;
+            newPos.x = 7.013509f;
+            transform.position = newPos;
+        }
+
+        if (transform.position.x < -7.03098f)
+        {
+            var newPos = transform.position;
+            newPos.x = -7.03098f;
+            transform.position = newPos;
+        }
+
+        if (transform.position.y > 3.753351f)
+        {
+            var newPos = transform.position;
+            newPos.y = 3.753351f;
+            transform.position = newPos;
+        }
+
+        if (transform.position.y < -3.72366f)
+        {
+            var newPos = transform.position;
+            newPos.y = -3.743805f;
+            transform.position = newPos;
+        }
     }
 }
