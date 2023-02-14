69ec3cf910f57796dde9b1dfd4c2f14a80950201
DESKTOP-TJTEG8U\Happy <mark.silva@happygrupo.com.br>
2023-02-14 15:35:57 -0300

    Movimentação e respawn do inimigo

--- Assets/Scripts/Inimigo.cs
+++ Assets/Scripts/Inimigo.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Inimigo : MonoBehaviour
 {
+    public float speed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(speed * Vector3.down * Time.deltaTime);
+        LimiteInimigo();
+    }
+    private void LimiteInimigo()
+    {
+        if (transform.position.y < -7.25479f)
+        {
+            var newPos = transform.position;
+            newPos.y = 5.25479f;
+            newPos.x = Random.Range(-4, 8);
+            transform.position = newPos;
+        }
     }
 }
